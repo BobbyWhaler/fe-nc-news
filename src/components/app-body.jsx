b6b598051a854bcda92a4ace920d93899c27b374
bobby.whaler@icloud.com <you@example.com>
2023-01-09 15:22:28 +0000

    task 4

---
+++ src/components/app-body.jsx
@@ -0,0 +1,18 @@
+// import { useState } from "react";
+import Header from './app-header'
+import ArticlesFeed from './article-feed'
+// import CommentContainer from './comment-container'
+// import NewCommentContainer from './new-comment-container'
+
+function AppBody() {
+return (
+<div>
+    <Header/>
+    <ArticlesFeed/>
+    {/* <CommentsContainer/>
+    <NewCommentContainer/> */}
+</div>
+)
+}
+
+export default AppBody
